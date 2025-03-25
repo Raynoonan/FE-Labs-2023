ba2a28fc8cf4f5d8a6194d1e7ee7b3b05db07235
Ray Noonan <raynoonan03@gmail.com>
2025-03-25 12:27:20 -0400

    Week04 Lab

--- Lab04/JS1VariablesAndOperations.js
+++ Lab04/JS1VariablesAndOperations.js
@@ -52,22 +52,28 @@ Question 1. Creating & 'console.log()'ing variables \n`);
  * ↓ YOUR CODE HERE ↓ */
 
 console.log("Step 1:  Number of Seats on a Plane");
-
+var availablePlaneSeats = 12;
+console.log("The available seats left are " + availablePlaneSeats);
 
 console.log("Step 2:  Cost of Groceries");
-
+var costOfGroceries = 40.50;
+console.log("The total cost is " + costOfGroceries);
 
 console.log("Step 3:  Middle Initial");
-
+var middleInitial = "J";
+console.log("Their middle initial is " + middleInitial);
 
 console.log("Step 4:  Is it Hot Outside?");
-
+var isItHot = true;
+console.log(isItHot + ", it is hot ouside");
 
 console.log("Step 5:  First Name");
-
+var customersFirstName = "Julien";
+console.log("The customer's first name is "+ customersFirstName);
 
 console.log("Step 6:  Street Address");
-
+var streetAddress = "124 neville ave";
+console.log("The street address is "+ streetAddress);
 
 /*-------------------------------------------------------*/
 
@@ -85,34 +91,36 @@ Question 2. Creating & 'console.log()'ing variables \n`);
     console.log('Remove Plane Seats:')
 //	A customer booked 2 plane seats. Remove 2 seats from the available seats variable
 //  ↓ YOUR CODE HERE ↓
-
+    console.log("A customer booked 2 seats so there are only " + (availablePlaneSeats - 2) + " left");
 
     console.log('Buy a Candy Bar:')
 //	Impulse candy bar purchase! Add 2.15 to the grocery total
 //  ↓ YOUR CODE HERE ↓
-
+    console.log("The total is now " + (costOfGroceries + 2.15));
 
     console.log('Fix a Birth Certificate:')
 //	A birth certificate was printed incorrectly, change the middle initial to something else
 //  ↓ YOUR CODE HERE ↓
-
+    console.log("Their middle initial is now " + (middleInitial = 'F'));
 
     console.log('Changing Seasons:')
 //	The season has changed, update the hot outside variable	to be opposite of what it was
 //  ↓ YOUR CODE HERE ↓
-    
+    isItHot = !isItHot
+    console.log("The season has changed so isItHot is " + isItHot);
 
     console.log('Full Name:')
 //	Create a new variable called full name using the customer's
 //  first name, the middle initial, and a last name of your choice
 //  ↓ YOUR CODE HERE ↓
-
+    var customerFullName = customersFirstName + " " + middleInitial + " Newman"
+    console.log("The customer's name is " + customerFullName);
 
     console.log('Introduce a Customer:')
 //	Print a line to the console that introduces the customer 
 //	including the name, and saying that they live at the address variable
 //  ↓ YOUR CODE HERE ↓
-
+   console.log("The customer " + customerFullName + " Lives at " + streetAddress);
 
 
     console.log('-----------Finished------------')
