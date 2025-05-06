5c2f917116a386869774f89ba7ebb0ab6f8b5a65
Ray Noonan <raynoonan03@gmail.com>
2025-05-06 16:00:34 -0400

    week 10 lab

--- Lab10/week10Lab.js
+++ Lab10/week10Lab.js
@@ -41,10 +41,32 @@ Part 1: Create a form with only JavaScript`)
            You should now have a label and a input on your website.
            Do the same steps, appending a label/input for Email, and a submit button to your form.
  * ↓ YOUR CODE HERE ↓ */
+let myForm = document.createElement('form')
+let myLabel = document.createElement('label')
+let myInput = document.createElement('input')
 
-// console.log(myForm)
+myLabel.innerHTML = 'Name';
+
+myForm.append(myLabel)
+myForm.append(myInput)
+document.body.append(myForm)
+
+let myEmailLabel = document.createElement('label')
+let myEmailInput = document.createElement('input')
+
+myEmailLabel.innerHTML = 'Email'
+
+mySubmitButton = document.createElement('button')
+mySubmitButton.innerHTML = 'Submit'
+
+myForm.append(myEmailLabel)
+myForm.append(myEmailInput)
+myForm.append(mySubmitButton)
+
+console.log(myForm)
+
 //Question: Besides changing the innerHTML, what other HTML things can we change using JavaScript?
-//Answer:
+//Answer: Besides the innerHTML, you can change the style of your page and the layout.
 
 /*------------------------ Styling a form with only JavaScript------------------------*/
 console.log(`-------------------------- 
@@ -59,9 +81,15 @@ Part 2: Styling our form with only JavaScript`)
  *  Step 3: Give your form a display of flex, a flex-direction of column, and a max-width of 250px
  *
  * ↓ YOUR CODE HERE ↓ */
+    myForm.style.backgroundColor = 'rgb(157, 218, 148)'
+    myForm.style.color = 'rgb(16, 100, 71)'
+
+    myForm.style.display = 'flex'
+    myForm.style.flexDirection = 'column'
+    myForm.style.maxWidth = '250px'
 
 //Question: Do you prefer styling with JavaScript or CSS?
-//Answer:
+//Answer: Styling with JavaScript makes a little more sense to me. 
 
 /*------------------------ Creating a table with only JavaScript ------------------------*/
 console.log(`-------------------------- 
@@ -80,10 +108,42 @@ Part 3: Creating a table with only JavaScript`)
  * Final Step: Style your table using JavaScript starting with a border. You may need to target more element/nodes than we did with myTable.
  * ↓ YOUR CODE HERE ↓ */
 
-// console.log(myTable)
+          
+let myTable = document.createElement('table')
+let myTr = document.createElement('tr')
+let myTdName = document.createElement('td')
+let myTdEmail = document.createElement('td')
+
+myTdName.innerHTML = 'Name'
+myTdEmail.innerHTML = 'Email'
+
+myTable.append(myTr)
+myTr.append(myTdName)
+myTr.append(myTdEmail)
+document.body.append(myTable)
+
+let newRow = myTable.insertRow(1)
+
+const cell1 = newRow.insertCell(0)
+const cell2 = newRow.insertCell(1)
+
+cell1.textContent = "Ray"
+cell2.textContent = "Ray@yahoo.com"
+
+let secondRow = myTable.insertRow(2)
+
+const cell3 = secondRow.insertCell(0)
+const cell4 = secondRow.insertCell(1)
+cell3.textContent = "Josh"
+cell4.textContent = "Josh@gmail.com"
+
+myTable.style.border = '1px solid black'
+
+console.log(myTable)
+
 //Question: While this is clearly a more round-about way of making a table, how can using JavaScript in this way
 //          more beneficial than HTML?
-//Answer:
+//Answer: Using JavaScript for making a table could be more beneficial for making live changes
 
 /*------------------------ Changing our body background on click ------------------------*/
 console.log(`-------------------------- 
@@ -103,10 +163,15 @@ Part 4: Changing our background on click`)
  * Step 7: Click the button in your index.html and see the background change!
  *
  * ↓ YOUR CODE HERE ↓ */
+const myButton = document.getElementById('myButton')
+
+myButton.addEventListener('click', function(){
+   document.body.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+})
 
 //Question: What other event listeners are there?
 //             Try them out by replacing 'click' in your function above!
-//Answer:
+//Answer: mouseover, mouseout, resize, 
 
 console.log(`-----------Finished------------`)
 
